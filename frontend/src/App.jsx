1f73a66e7af9f4b2d54cd1ade13ff63613a1ba40
ray31-source <ventureforecastofficial@gmail.com>
2025-07-08 13:04:10 +0000

    Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+   <div className="h-screen w-full flex flex-col justify-center items-center">
+    <form>
+    <input className="border-none border-black-900 rounded-lg" placeholder="Enter Your Name"></input>
+    <button></button>
+    </form>
+   </div>
+    </>
+  )
+}
+
+export default App
