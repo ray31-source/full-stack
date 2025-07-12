ff5d1455d96bf71b88566af038a6a4547d914fe3
ray31-source <ventureforecastofficial@gmail.com>
2025-07-12 12:13:06 +0000

    Backend OK

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app =express()
 const userRouter = require('./users/userRouter')
 app.use(express.json())
+app.use(cors())
 app.use('/user',userRouter)
 app.get('/',(req,res)=>{
     res.send("It is the Home Page")
