317e72d18b76a76599ee3535ddfabcbd04a1bd96
ray31-source <ventureforecastofficial@gmail.com>
2025-07-13 13:02:49 +0000

    Nothing

--- backend/users/userRouter.js
+++ backend/users/userRouter.js
@@ -4,9 +4,11 @@ userRouter.post('/sign-up',(req,res)=>{
    const {username,password} = req.body
    if(!username || !password){
     console.log("User Credentials are needed");
+    res.status(401).send("Unauthorized")
     
    }
    console.log("User Created");
+   res.status(200).send('Successful')
    
 })
 module.exports = userRouter
